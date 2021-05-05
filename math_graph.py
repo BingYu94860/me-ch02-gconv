5c41958b83b8cdfe71a2810c8bf1233344bf7a1e
BingYu <33444256+BingYu94860@users.noreply.github.com>
2021-05-05 16:49:01 +0800

    update math graph and hypergraph
    update math graph and hypergraph

---
+++ math_graph.py
@@ -0,0 +1,69 @@
+import numpy as np
+import scipy.sparse as sp
+
+
+def get_edges(sparse_matrix, is_triu=True):
+    coo = sp.coo_matrix(sparse_matrix)
+    if is_triu:
+        coo = sp.triu(coo, 1)
+    return np.vstack((coo.row, coo.col)).transpose()  # .tolist()
+#==========#==========#==========#==========#==========#==========#==========#
+# 產生鄰接矩陣adj
+
+
+def get_adj(edges: list, num_nodes: int):
+    e_rows, e_cols = np.array(edges, dtype=np.int).transpose()
+    values = np.ones(shape=(len(e_rows), ), dtype=np.float32)
+    adj = sp.coo_matrix((values, (e_rows, e_cols)),
+                        shape=[num_nodes, num_nodes])
+    # triu adj --> adj
+    adj.setdiag(0)
+    bigger = adj.T > adj
+    adj = adj - adj.multiply(bigger) + adj.T.multiply(bigger)
+    return adj
+
+
+#==========#==========#==========#==========#==========#==========#==========#
+# 從 adj 計算出 D^(-0.5) @ (I+adj) @D^(-0.5)
+
+def get_sp_DAD(adj, mode=1):
+    N = adj.shape[-1]
+    if mode == 1:  # 拉普拉斯平滑化
+        A = sp.eye(N) + sp.coo_matrix(adj)
+    elif mode == 2:  # 拉普拉斯銳利化
+        A = 2 * sp.eye(N) - sp.coo_matrix(adj)
+    else:
+        A = sp.coo_matrix(adj)
+    # 幾何平均
+    D = np.power(np.sum(np.abs(A), -1), -0.5)
+    DAD = A.multiply(D).T.multiply(D)
+    return DAD
+
+#==========#==========#==========#==========#==========#==========#==========#
+# 從 adj 計算出 正規化拉普拉斯 L_norm
+
+
+def get_sp_L_norm_from_adj(adj):
+    N = adj.shape[-1]
+    a = sp.coo_matrix(adj)
+    D = np.power(np.sum(np.abs(a), -1), -0.5)
+    DaD = get_sp_DAD(adj, mode=1)
+    L_norm = sp.eye(N) - DaD
+    return L_norm
+
+
+#==========#==========#==========#==========#==========#==========#==========#
+# 將 L_norm 做chebyshev輸入的正規化
+
+def get_sp_L_chebyshev_norm_from_adj(adj, lambda_max=2.0):
+    N = adj.shape[-1]
+    a = sp.coo_matrix(adj)
+    D = np.power(np.sum(np.abs(a), -1), -0.5)
+    DaD = a.multiply(D).T.multiply(D)
+    # chebyshev norm
+    if lambda_max == 2:
+        L_chebyshev_norm = -DaD
+    else:
+        L_norm = sp.eye(N) - DaD
+        L_chebyshev_norm = (2.0 / lambda_max) * L_norm - sp.eye(N)
+    return L_chebyshev_norm
---
+++ math_hypergraph.py
@@ -0,0 +1,90 @@
+import numpy as np
+import scipy.sparse as sp
+
+#==========#==========#==========#==========#==========#==========#==========#
+# 產生連接矩陣H
+
+
+def get_np_H(hyperedges: list, num_nodes: int):
+    arr = np.arange(num_nodes)
+    H_T = []
+    for edge in hyperedges:
+        arr1 = np.tile(arr, (len(edge), 1))
+        arr2 = np.tile(edge, (num_nodes, 1)).transpose()
+        v_edge = (arr1 == arr2).astype(np.int).sum(0)
+        H_T.append(v_edge)
+    H = np.array(H_T).transpose()
+    return H.astype(np.float32)
+
+
+def get_sp_H(hyperedges: list, num_nodes: int):
+    num_hyperedges = len(hyperedges)
+    e_rows = np.array([node for edge in hyperedges for node in edge])
+
+    num_nodes_list = [len(edge) for edge in hyperedges]
+    e_cols = np.repeat(range(len(num_nodes_list)), num_nodes_list)
+
+    values = np.ones(shape=(len(e_rows)))
+
+    H = sp.coo_matrix((values, (e_rows, e_cols)),
+                      shape=[num_nodes, num_hyperedges])
+    return H.astype(np.float32)
+
+#==========#==========#==========#==========#==========#==========#==========#
+# 從 H 計算出 鄰接矩陣adj
+
+
+def get_sp_adj_from_H(H, W=None):
+    num_nodes, num_hyperedges = H.shape
+    if W is None:
+        W = np.ones(shape=[num_hyperedges])
+    W = np.reshape(W, [1, num_hyperedges])
+    Dv = np.sum(H.multiply(W), -1)  # (num_nodes, 1)
+    Dv = sp.diags(np.squeeze(np.asarray(Dv)))
+    # H @ W @ H.T
+    HWH = H.multiply(W) @ H.T
+    adj = HWH - Dv
+    return adj
+
+#==========#==========#==========#==========#==========#==========#==========#
+# 從 H 計算出 Dv^(-0.5)@H @ W@De^(-1) @ H@Dv^(-0.5)
+
+
+def get_sp_DvH_WDe_HDv(H, W=None):
+    num_nodes, num_hyperedges = H.shape
+    if W is None:
+        W = np.ones(shape=[num_hyperedges])
+    W = np.reshape(W, [1, num_hyperedges])
+    De = np.sum(H, -2)  # (1, num_hyperedges)
+    Dv = np.sum(H.multiply(W), -1)  # (num_nodes, 1)
+
+    # WDe = W @ De^(-1)
+    WDe = np.multiply(W, np.power(De, -1))
+    # DvH = Dv^(-0.5) @ H
+    DvH = H.multiply(np.power(Dv, -0.5))
+    # DvH @ WDe @ HDv
+    DvH_WDe_HDv = DvH.multiply(WDe) @ DvH.T
+    return DvH_WDe_HDv
+
+#==========#==========#==========#==========#==========#==========#==========#
+# 從 H 計算出 正規化拉普拉斯 L_norm
+
+
+def get_sp_L_norm_from_H(H, W=None):
+    num_nodes, num_hyperedges = H.shape
+    DvH_WDe_HDv = get_sp_DvH_WDe_HDv(H, W)
+    L_norm = sp.eye(num_nodes) - DvH_WDe_HDv
+    return L_norm
+
+
+#==========#==========#==========#==========#==========#==========#==========#
+def get_sp_L_chebyshev_norm_from_H(H, W=None, lambda_max=2.0):
+    num_nodes, num_hyperedges = H.shape
+    DvH_WDe_HDv = get_sp_DvH_WDe_HDv(H, W)
+    # chebyshev norm
+    if lambda_max == 2:
+        L_chebyshev_norm = -DvH_WDe_HDv
+    else:
+        L_norm = sp.eye(num_nodes) - DvH_WDe_HDv
+        L_chebyshev_norm = (2.0 / lambda_max) * L_norm - sp.eye(N)
+    return L_chebyshev_norm
